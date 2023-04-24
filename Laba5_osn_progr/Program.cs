02ef3e46a4f8f7023747dbbff88a5240455b5c9a
Yassenovich <119537172+Yassenovich@users.noreply.github.com>
2023-04-24 13:13:34 +0300

    Добавьте файлы проекта.

---
+++ Laba5_osn_progr/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.Write("Введіть текст: ");
+        string text = Console.ReadLine();
+
+        char[] chars = text.ToCharArray();
+        int maxCount = 0;
+        char maxChar = '\0';
+
+        foreach (char c in chars)
+        {
+            int count = 0;
+            foreach (char c2 in chars)
+            {
+                if (c == c2)
+                {
+                    count++;
+                }
+            }
+            if (count > maxCount)
+            {
+                maxCount = count;
+                maxChar = c;
+            }
+        }
+
+        Console.WriteLine($"Найбільша кількість однакових символів: {maxCount}, символ: {maxChar}");
+    }
+}
+
